021a5856b2b1908d064568620bf740ebef45267d
Zul Wasaya <zul.wasaya@gmail.com>
2013-10-22 14:29:28 +0100

    added secret key to devise.rb initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,4 +243,8 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+
+
+  # Added secret key
+  config.secret_key = '581920ee4c73e1433cd667298027d2698336a1ccc15430bfe7582a620adecef3cb23f625498c28881796d32d9ae839acefdc078839d07b62dd8d8fc90b9c6c86'
 end
